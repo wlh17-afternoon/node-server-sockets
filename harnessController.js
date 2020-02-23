40dfd9bd9553e601b1f60face3f3189b73b04cd0
rob lingstuyl <808numbers@gmail.com>
2020-02-22 21:03:25 -0700

    added more resolution to clients and server to track devices.  Changed output directories.

--- harnessController.js
+++ harnessController.js
@@ -1,28 +1,25 @@
 //Run this file with command 'node harnessController.js'
 //File permissions may need to be changed for this to execute
 
-
 const { exec, execFile } = require("child_process");
 const _ = require("lodash");
 
-
-let newId = 12345;
 const number = _.range(20);
 const outputHandler = (error, stdout, stderr) => {
-  console.error(error)
-  console.log(stderr)
-}
+  console.error(error);
+  console.log(stderr);
+};
 
-exec(`nodemon`, outputHandler)
+// exec(`nodemon`, outputHandler);
 
 number.map(el => {
-  exec(`node src/controllers/clientController.js ${el + newId}`, outputHandler);
+  exec(`pwd`);
+  exec(`node src/controllers/clientController.js ${el}`, outputHandler);
 });
 
-
-setTimeout(()=> {
+setTimeout(() => {
   number.map(el => {
-    exec(`killall node`, outputHandler)
-  })
-    exec(`killall nodemon`)
-}, 1000 * 60)
+    exec(`killall node`, outputHandler);
+  });
+  // exec(`killall nodemon`);
+}, 1000 * 60);
