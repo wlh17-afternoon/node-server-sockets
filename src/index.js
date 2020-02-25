74fc12d71340073cc3c6accf24d1d7203ebc8279
mutantrobbyk <robertknowles02@msn.com>
2020-02-24 21:18:05 -0700

    made changes to port

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const socket = require('socket.io')
 const ssl = require('./controllers/socketController')
 
 
-const io = socket(app.listen(3550, () => console.log('Listening on port 3550')))
+const io = socket(app.listen(3551, () => console.log('Listening on port 3551')))
 
 io.on('connection', socket => {
     // console.log('A new user just connected')
